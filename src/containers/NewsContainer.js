fff5bb7b7058c57bca45cb09af26794e8c86ed01
mjmccoach <mmccoach88@gmail.com>
2020-12-09 12:38:53 +0000

    Close to having apis talking to each other, run into rogue brackets

--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -6,23 +6,36 @@ import { useState, useEffect } from "react";
 const NewsContainer = () => {
 
     const [news, setNews] = useState([])
+    const [story, setStory] = useState({})
+    const [selectedStoryId, setSelectedStoryId] = useState({})
 
     useEffect(() => {
     getNews()},[]
     )
 
+    // 25344354 - use this for checking API
     const getNews = () => {
         fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
         .then(res => res.json())
         .then(news => setNews(news))
+        .then(news.forEach(selectedStoryId => {
+            fetch(`https://hacker-news.firebaseio.com/v0/item/${selectedStoryId}.json`)
+            .then(res => res.json())
+            .then(story => setStory(story))
+
+            Promise.all([news, story]).then((values) => {
+                console.log(values)
+            }
+            
+        ))
     }
 
-    
 
     return (
         <>
         <h1>This is the news container....</h1>
         <Title/>
+
         <StoryList/>
         </>
     );
