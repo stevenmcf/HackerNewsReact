440928a0253ba38a7bcf2c3d11ce3c5f9dec39ad
mjmccoach <mmccoach88@gmail.com>
2020-12-09 11:46:10 +0000

    Hooked in ID API successfully

--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -1,14 +1,28 @@
 import Filter from "../components/Filter";
 import StoryList from "../components/StoryList";
 import Title from "../components/Title";
+import { useState, useEffect } from "react";
 
 const NewsContainer = () => {
+
+    const [news, setNews] = useState([])
+
+    useEffect(() => {
+    getNews()},[]
+    )
+
+    const getNews = () => {
+        fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+        .then(res => res.json())
+        .then(news => setNews(news))
+    }
+
+    
 
     return (
         <>
         <h1>This is the news container....</h1>
         <Title/>
-        
         <StoryList/>
         </>
     );
